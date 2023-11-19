e743ff74a89695c2941e1c1186920deb50181518
lnhang <654344283@qq.com>
2023-11-19 20:56:56 +0800

    update sh

---
+++ Utilities/dafoam/loadmydafoam.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+# 设置 plot 脚本的路径
+PLOT_PATH="$(pwd)/scripts/plot"
+export PATH="$PLOT_PATH:$PATH"
+
+# 设置 createSDict 脚本的路径
+CREATESDICT_PATH="$(pwd)/scripts/createSDict"
+export PATH="$CREATESDICT_PATH:$PATH"
