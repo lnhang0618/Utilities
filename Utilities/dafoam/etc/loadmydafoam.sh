975a942d793bfbda62563fadae8125ea0bc4ae9f
lnhang <654344283@qq.com>
2023-11-19 20:46:11 +0800

    update envir

--- Utilities/dafoam/etc/loadmydafoam.sh
+++ Utilities/dafoam/etc/loadmydafoam.sh
@@ -1,5 +1,12 @@
-# Loads the plot scripts
-export PATH="../scripts/plot:$PATH"
+#!/bin/bash
 
-# Loads the sampleDict scripts
-export PATH="../scripts/createSDict:$PATH"
+# 获取脚本所在的目录
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# 设置 plot 脚本的路径
+PLOT_PATH="$SCRIPT_DIR/scripts/plot"
+export PATH="$PLOT_PATH:$PATH"
+
+# 设置 createSDict 脚本的路径
+CREATESDICT_PATH="$SCRIPT_DIR/scripts/createSDict"
+export PATH="$CREATESDICT_PATH:$PATH"
