d7521d949e5fe0eca52d5e41344e568ba6058703
lnhang <654344283@qq.com>
2023-11-19 21:17:02 +0800

    update sh

--- Utilities/dafoam/loadmydafoam.sh
+++ Utilities/dafoam/loadmydafoam.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# 输出当前工作目录
+echo "Current working directory: $(pwd)"
 
-# 设置 plot 脚本的路径
+# 设置脚本路径
 scripts_PATH="$(pwd)/scripts/"
-export PATH="$scripts_PATH:$PATH"
 
+# 检查scripts目录是否存在
+if [ -d "$scripts_PATH" ]; then
+    echo "Found scripts directory: $scripts_PATH"
+    export PATH="$scripts_PATH:$PATH"
+    echo "PATH updated to include scripts directory."
+else
+    echo "Scripts directory not found: $scripts_PATH"
+fi
 
+# 输出新的PATH以供检查
+echo "Current PATH: $PATH"
