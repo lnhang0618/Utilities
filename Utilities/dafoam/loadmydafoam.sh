36c20e85816a97f21eabbd8c1f1e5345d001bf67
lnhang <654344283@qq.com>
2023-11-19 22:46:08 -0500

    update env bash

--- Utilities/dafoam/loadmydafoam.sh
+++ Utilities/dafoam/loadmydafoam.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# 输出当前工作目录
-echo "Current working directory: $(pwd)"
-
-# 设置脚本路径
-scripts_PATH=$(pwd)/scripts/
+# 获得scripts目录的路径
+WORK_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+SCRIPTS_PATH="$WORK_PATH/scripts"
 
 # 检查scripts目录是否存在
-if [ -d "$scripts_PATH" ]; then
-    echo "Found scripts directory: $scripts_PATH"
-    export PATH=$scripts_PATH:$PATH
+if [ -d "$SCRIPTS_PATH" ]; then
+    echo "Found scripts directory: $SCRIPTS_PATH"
+    export PATH=$SCRIPTS_PATH:$PATH
     echo "PATH updated to include scripts directory."
 else
-    echo "Scripts directory not found: $scripts_PATH"
+    echo "Scripts directory not found: $SCRIPTS_PATH"
 fi
 
 # 输出新的PATH以供检查
